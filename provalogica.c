4ecb775dd9dc6bed78399b18d351b32b13d3b776
Dora <161501828+isadorabarboza@users.noreply.github.com>
2024-11-11 15:40:59 -0300

    Add files via upload

---
+++ provalogica.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+ struct Contato {
+    char nome[100];
+    int numero;
+ };
+
+int main(){
+ int opcao = -1;
+ int opcao2 = -1;
+
+ struct Contato agenda [50];
+
+ while(opcao!=0){
+ 
+         printf("aperte 0 para sair do loop\n");
+          printf("aperte 1 para ligar para a policia\n");
+           printf("aperte 2 para ligar para o samu\n");
+            printf("aperte 3 para ligar para o bombeiro\n");
+             scanf("%d",&opcao);
+             
+              if (opcao == 1){
+                   printf("ligando para a policia\n");
+                     printf("aperte 0 para desligar a ligação\n");
+                      scanf("%d",&opcao2);
+              }else if (opcao ==2){
+                    printf("escreva seu nome completo:");
+                    scanf("")
+                    printf("lugando para o samu\n");
+                    printf("aperte 0 para desligar a ligação\n");
+                    scanf("%d",&opcao2);
+              }else if (opcao == 3){
+                  printf("ligando para o bombeiro\n");
+                   printf("aperte 0 para desligar a ligação\n");
+                   scanf("%d",&opcao2);
+              }
+}
+ return 0;
+}
